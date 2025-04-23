f4451f7cbf0bf77b61153bc9ed9151e580dfc94b
[Jihwan <[kjhcan00303@gmail.com]>
2025-04-23 12:49:37 +0000

    2

--- For_exam/z.cpp
+++ For_exam/z.cpp
@@ -1,11 +1,28 @@
-#include <iostream>
-using namespace std; 
-int main()
-{
-    char a,b;
-    a=0x10;
-    b=0xFB;
+// #include <iostream>
+// using namespace std; 
+// int main()
+// {
+//     int a,b;
+//     unsigned int c,d;
 
-    cout << a, b <<endl
+//     a=0x10;
+//     b=0xFB;
+//     c=0x10;
+//     d=0xFB;
+
+//     cout << "a, b의 값은? " << a << ", " << b << endl;
+//     cout << "c, d의 값은? " << c << ", " << d << endl;
+//     return 0;
+// }
+
+#include <iostream>
+using  namespace std;
+int main() {
+    long a;
+    uint64_t b;
+    a=0x1234567890;
+    b=0x1234567890;
+    cout << "a: "<< a << endl;
+    cout << "b: "<< b <<endl;
     return 0;
 }
